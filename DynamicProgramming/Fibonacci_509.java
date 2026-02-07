286ee9d8bcc92fb4395430b7190e756b4cfdf0df
Mahabub Ali <alimahabub977@gmail.com>
2026-02-07 10:13:32 +0530

    Add 509 - Fibonacci Number (Space Optimized DP)

---
+++ DynamicProgramming/Fibonacci_509.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+public class Fibonacci_509 {
+    public static int fibpnacci(int n) {
+        // corner case
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        int prev1 = 0; // f(0)
+        int prev2 = 1;// f(1)
+        // int curr = 0;// f(n)
+        // loop
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+
+    public static void main(String args[]) {
+        int n = 4;
+        System.out.println(fibpnacci(n));
+    }
+
+}
